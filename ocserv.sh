72c6243d68806a7e79d3dd3c9f25a971bb233ce5
Shang Zhaodi <lgdglgc@gmail.com>
2024-11-24 18:43:34 +0800

    Update ocserv.sh

--- ocserv.sh
+++ ocserv.sh
@@ -18,8 +18,8 @@ install_ocserv(){
 edit_conf(){
 	echo -n "Server Address " > /etc/ocserv/server.address
 	echo -n $1 >> /etc/ocserv/server.address
-	echo ":3389(Port MUST Not Blocked)" >> /etc/ocserv/server.address
-	curl -o /etc/ocserv/ocserv.conf https://raw.githubusercontent.com/githik999/ocserv_one_key/main/ocserv.conf 
+	echo ":8443(Port MUST Not Blocked)" >> /etc/ocserv/server.address
+	curl -o /etc/ocserv/ocserv.conf https://raw.githubusercontent.com/lgdglgc/ocserv888/main/ocserv.conf 
 	echo "no-route=$1/24" >> /etc/ocserv/ocserv.conf
 }
 
